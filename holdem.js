88edbc6c0fa2baa40d46b035913851676db3077b
Ali, Muhammad <mali301@gatech.edu>
2021-04-14 15:36:43 -0600

    some more tests added. functions for checking pairs, 3-4 of a kind added

--- holdem.js
+++ holdem.js
@@ -1,16 +1,46 @@
-console.log('Start');
-
-
 
 var checkAllEqual = (arr) => arr.every(card => card == arr[0])
 
-var a1 = (arr) => arr.every(card => card == arr[0])
+var countOccurrence = (arr) => {
+    var count = {};
+    arr.forEach(element => {
+        count[element] = (count[element] + 1) || 1;
+    });
+    return count;
+}
 
-// module.exports = checkAllEqual;
+var isCountPresent = (arr, num) => {
+    var count = countOccurrence(arr);
+    console.log(count);
+    if (Object.values(count).includes(num)){
+        return true
+    }
+    else{
+        return false;
+    }
+    
+}
+
+var isPairPresent = (arr) => isCountPresent(arr, 2)
+
+var isThreeOfAKindPresent = (arr) => isCountPresent(arr, 3)
+
+// var isFourOfAKindPresent = (arr) => isCountPresent(arr, 4)
+
+
+
+console.log(isCountPresent([1,1,2,2,3,4,5], 3))
+
+
+
 
 module.exports = {
     checkAllEqual: checkAllEqual,
-    a1: a1
+    countOccurrence: countOccurrence,
+    isCountPresent: isCountPresent,
+    isPairPresent: isPairPresent,
+    isThreeOfAKindPresent: isThreeOfAKindPresent
+    // a1: a1
 }
 
 
--- holdem.test.js
+++ holdem.test.js
@@ -1,18 +1,39 @@
+const { expect } = require('@jest/globals')
 const holdem = require('./holdem')
 
-test('Check all suits are same', () =>{
-    expect(holdem.checkAllEqual(['S','S','S','S','S'])).toBe(true)
+test('Check all suits are same', () => {
+    expect(holdem.checkAllEqual(['S', 'S', 'S', 'S', 'S'])).toBe(true)
 })
 
-test('Check if all faces are same', () =>{
-    expect(holdem.checkAllEqual([8,8,8,8,8])).toBe(true)
+test('Check if all faces are same', () => {
+    expect(holdem.checkAllEqual([8, 8, 8, 8, 8])).toBe(true)
 })
 
-test('Check if suits are different', () =>{
-    expect(holdem.checkAllEqual(['S','S','C','S','D'])).toBe(false)
+test('Check if suits are different', () => {
+    expect(holdem.checkAllEqual(['S', 'S', 'C', 'S', 'D'])).toBe(false)
 })
 
-test('Check if all faces are different', () =>{
-    expect(holdem.checkAllEqual([8,8,7,8,8])).toBe(false)
+test('Check if all faces are different', () => {
+    expect(holdem.checkAllEqual([8, 8, 7, 8, 8])).toBe(false)
+})
+
+test('Count Occurrence set 1', () => {
+    expect(holdem.countOccurrence([2, 3, 4, 5, 8])).toStrictEqual({ '2': 1, '3': 1, '4': 1, '5': 1, '8': 1 })
+})
+
+test('Count Occurrence set 2', () => {
+    expect(holdem.countOccurrence([5, 5, 5])).toStrictEqual({ '5': 3 })
+})
+
+test('Count Occurrence set 3', () => {
+    expect(holdem.countOccurrence([2, 2, 3, 6, 7, 7])).toStrictEqual({ '2': 2, '3': 1, '6': 1, '7': 2 })
+})
+
+test('Is Pair Present', () => {
+    expect(holdem.isPairPresent([2, 2, 3, 6, 7, 7])).toBe(true)
+})
+
+test('Is Three of a Kind Present', () => {
+    expect(holdem.isThreeOfAKindPresent([2, 2, 3, 6, 7, 7])).toBe(false)
 })
 
