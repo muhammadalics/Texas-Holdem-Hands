13b9fbcf36e340f01ad0b154b4a0b6672198e8b0
Ali, Muhammad <mali301@gatech.edu>
2021-04-14 22:41:57 -0600

    some refactoring

--- holdem.js
+++ holdem.js
@@ -1,3 +1,4 @@
+const { arrayExpression } = require("@babel/types");
 
 var checkAllEqual = (arr) => arr.every(card => card == arr[0])
 
@@ -11,27 +12,58 @@ var countOccurrence = (arr) => {
 
 var isCountPresent = (arr, num) => {
     var count = countOccurrence(arr);
-    console.log(count);
-    if (Object.values(count).includes(num)){
-        return true
-    }
-    else{
-        return false;
-    }
-    
+    console.log(count); 
+    return Object.values(count).includes(num);   
 }
 
 var isPairPresent = (arr) => isCountPresent(arr, 2)
 
 var isThreeOfAKindPresent = (arr) => isCountPresent(arr, 3)
 
-// var isFourOfAKindPresent = (arr) => isCountPresent(arr, 4)
+var isFourOfAKindPresent = (arr) => isCountPresent(arr, 4)
+
+var twoPairsPresent = (arr) => {
+    var count = countOccurrence(arr);
+    var pairCount = 0;
+    Object.values(count).forEach(number => {
+        if (number == 2){
+            pairCount++;
+        }
+    })
+    return pairCount == 2;
+}
+
+var deleteOneIfPresent = (arr) => {
+    indexOfOne = arr.indexOf(1); //Check if Ace is present
+    if ( indexOfOne != -1 ){
+        arr.splice(indexOfOne, 1)
+    }
+    return arr;
+}
+
+var areNumbersInSequence = (arr) => {
+    arr = deleteOneIfPresent(arr);
+    sortedArray = arr.sort();
+    subtractionArray = [];
+    for(let index=1; index < sortedArray.length; index++ ){
+        subtractionArray.push(sortedArray[index] - sortedArray[index -1]) // if numbers are in sequence then this array will be all 1.       
+    }
+    return checkAllEqual(subtractionArray) && subtractionArray[0] == 1;
+}
 
 
 
-console.log(isCountPresent([1,1,2,2,3,4,5], 3))
+var isFlush = (suits) => {
+    return checkAllEqual(suits)
+}
+
+var isHandPair = (arr) => {
+    return isPairPresent(arr) && twoPairsPresent(arr) == false ;
+}
 
 
+
+console.log(areNumbersInSequence([2,3,4,5,6, 1]))
 
 
 module.exports = {
@@ -39,7 +71,10 @@ module.exports = {
     countOccurrence: countOccurrence,
     isCountPresent: isCountPresent,
     isPairPresent: isPairPresent,
-    isThreeOfAKindPresent: isThreeOfAKindPresent
+    isThreeOfAKindPresent: isThreeOfAKindPresent,
+    isFourOfAKindPresent: isFourOfAKindPresent,
+    twoPairsPresent: twoPairsPresent,
+    areNumbersInSequence: areNumbersInSequence
     // a1: a1
 }
 
--- holdem.test.js
+++ holdem.test.js
@@ -17,23 +17,70 @@ test('Check if all faces are different', () => {
     expect(holdem.checkAllEqual([8, 8, 7, 8, 8])).toBe(false)
 })
 
-test('Count Occurrence set 1', () => {
+test('Count occurrence set 1', () => {
     expect(holdem.countOccurrence([2, 3, 4, 5, 8])).toStrictEqual({ '2': 1, '3': 1, '4': 1, '5': 1, '8': 1 })
 })
 
-test('Count Occurrence set 2', () => {
+test('Count occurrence set 2', () => {
     expect(holdem.countOccurrence([5, 5, 5])).toStrictEqual({ '5': 3 })
 })
 
-test('Count Occurrence set 3', () => {
-    expect(holdem.countOccurrence([2, 2, 3, 6, 7, 7])).toStrictEqual({ '2': 2, '3': 1, '6': 1, '7': 2 })
+test('Count occurrence set 3', () => {
+    expect(holdem.countOccurrence([2, 2, 3, 6, 7])).toStrictEqual({ '2': 2, '3': 1, '6': 1, '7': 1 })
 })
 
-test('Is Pair Present', () => {
-    expect(holdem.isPairPresent([2, 2, 3, 6, 7, 7])).toBe(true)
+test('Is pair present ?', () => {
+    expect(holdem.isPairPresent([2, 2, 6, 7, 7])).toBe(true)
 })
 
-test('Is Three of a Kind Present', () => {
-    expect(holdem.isThreeOfAKindPresent([2, 2, 3, 6, 7, 7])).toBe(false)
+test('Is pair present ?', () => {
+    expect(holdem.isPairPresent([2, 4, 3, 6, 8])).toBe(false)
 })
 
+test('Is three of a kind present', () => {
+    expect(holdem.isThreeOfAKindPresent([2, 3, 6, 7, 7])).toBe(false)
+})
+
+test('Is three of a kind present', () => {
+    expect(holdem.isThreeOfAKindPresent([2, 2, 2, 6, 7])).toBe(true)
+})
+
+test('Is four of a kind present ?', () => {
+    expect(holdem.isFourOfAKindPresent([6, 6, 6, 6, 7])).toBe(true)
+})
+
+test('Is four of a kind present ?', () => {
+    expect(holdem.isFourOfAKindPresent([6, 7, 2, 6, 7])).toBe(false)
+})
+
+test('Are two pairs present ?', () => {
+    expect(holdem.twoPairsPresent([6, 7, 2, 6, 7])).toBe(true)
+})
+
+test('Are two pairs present ?', () => {
+    expect(holdem.twoPairsPresent([6, 7, 7, 6, 7])).toBe(false)
+})
+
+test('Are two pairs present ?', () => {
+    expect(holdem.twoPairsPresent([7, 7, 7, 7, 7])).toBe(false)
+})
+
+test('Are two pairs present ?', () => {
+    expect(holdem.twoPairsPresent([9, 9, 2, 2, 2])).toBe(false)
+})
+
+test('Are numbers in sequence ?', () => {
+    expect(holdem.areNumbersInSequence([4, 6, 8, 7, 5])).toBe(true)
+})
+
+test('Are numbers in sequence ?', () => {
+    expect(holdem.areNumbersInSequence([2, 3, 3, 4, 5])).toBe(false)
+})
+
+test('Are numbers in sequence ?', () => {
+    expect(holdem.areNumbersInSequence([2, 3, 7, 4, 5])).toBe(false)
+})
+
+test('Are numbers in sequence ?', () => {
+    expect(holdem.areNumbersInSequence([1, 3, 7, 4, 5])).toBe(false)
+})
